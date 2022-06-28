6c19e247e40775411a398456176f71450a63df99
sanjo lawal <sanjolawal600@gmail.com>
2022-06-28 13:35:44 -0700

    commit

--- Script.js
+++ Script.js
@@ -1,3 +1,27 @@
+// splash page script
+
+const nav = document.querySelector("#nav");
+const header = document.querySelector("#header");
+const body = document.querySelector("#body");
+const password = document.querySelector("#password");
+const splashSubmit = document.querySelector("#splashSubmit");
+const splashContainer = document.querySelector("#splashContainer");
+
+const Prevent = (e) => {
+  e.preventDefault();
+  if (password.value === `${1234}#${56}#`) {
+    splashContainer.style.display = "none";
+    body.style.height = "";
+    body.style.overflow = "visible";
+    nav.style.display = "grid";
+    header.style.display = "block";
+  }
+};
+
+splashSubmit.addEventListener("click", Prevent);
+
+// NAVIGATION BAR OPENING AND CLOSING SCRIPT/
+
 const hamburger = document.querySelector("#hamburger");
 const closed = document.querySelector("#closed");
 console.log(closed);
@@ -6,11 +30,9 @@ const Run = () => {
   if (window.innerWidth < 750) {
     document.body.style.height = "100vh";
     document.body.style.overflow = "hidden";
-    const nav = document.querySelector("#nav");
     nav.style.height = "100vh";
     nav.style.background = "rgb(215,0,255, 1)";
     const navContainer = document.querySelector("#navContainer");
-    // console.log(navSub);
     navContainer.style.display = "block";
     navContainer.style.marginTop = "-35vh";
     closed.style.display = "grid";
@@ -36,7 +58,7 @@ const Hide = () => {
 
 closed.addEventListener("click", Hide);
 
-// POPUP SCRIPT SECTION
+// CONNECT WALLET POPUP SCRIPT SECTION
 
 const popUpClosed = document.querySelector("#popUpClosed");
 const navMenu1 = document.querySelector("#navMenu1");
@@ -54,3 +76,103 @@ const hidePopUp = () => {
 };
 
 popUpClosed.addEventListener("click", hidePopUp);
+
+// Cards section script and info 
+
+const data = [
+  {
+    name: "@johny",
+    title: "Lorem Ipsum",
+    bid: "Current Bid",
+    price: "0.005ETH",
+    img: "./Images/red love.svg",
+    cta: "Buy Now",
+  },
+  {
+    name: "@johny",
+    title: "Lorem Ipsum",
+    bid: "Current Bid",
+    price: "0.005ETH",
+    img: "./Images/red love.svg",
+    cta: "Buy Now",
+  },
+  {
+    name: "@johny",
+    title: "Lorem Ipsum",
+    bid: "Current Bid",
+    price: "0.005ETH",
+    img: "./Images/red love.svg",
+    cta: "Buy Now",
+  },
+  {
+    name: "@johny",
+    title: "Lorem Ipsum",
+    bid: "Current Bid",
+    price: "0.005ETH",
+    img: "./Images/red love.svg",
+    cta: "Buy Now",
+  },
+  {
+    name: "@johny",
+    title: "Lorem Ipsum",
+    bid: "Current Bid",
+    price: "0.005ETH",
+    img: "./Images/red love.svg",
+    cta: "Buy Now",
+  },
+  {
+    name: "@johny",
+    title: "Lorem Ipsum",
+    bid: "Current Bid",
+    price: "0.005ETH",
+    img: "./Images/red love.svg",
+    cta: "Buy Now",
+  },
+  {
+    name: "@johny",
+    title: "Lorem Ipsum",
+    bid: "Current Bid",
+    price: "0.005ETH",
+    img: "./Images/red love.svg",
+    cta: "Buy Now",
+  },
+  {
+    name: "@johny",
+    title: "Lorem Ipsum",
+    bid: "Current Bid",
+    price: "0.005ETH",
+    img: "./Images/red love.svg",
+    cta: "Buy Now",
+  },
+  {
+    name: "@johny",
+    title: "Lorem Ipsum",
+    bid: "Current Bid",
+    price: "0.005ETH",
+    img: "./Images/red love.svg",
+    cta: "Buy Now",
+  },
+];
+const newdata = data.map((each) => {
+  return `
+       <div class="singleCards">
+        <div class="cardsCon">
+          <artcile>
+            <p class="Opacity">${each.name}</p>
+            <p class="noOpacity">${each.title}</p>
+          </artcile>
+          <section>
+            <p class="Opacity1">${each.bid}</p>
+            <p class="noOpacity1">${each.price}</p>
+          </section>
+        </div>
+        <div class="mainBtn">
+          <img src="./Images/red love.svg" alt="redlove" loading="lazy" />
+          <button class="cardsBtn">${each.cta}</button>
+        </div>
+      </div> `;
+});
+
+const mainCards = document.querySelector("#mainCards");
+
+mainCards.innerHTML = newdata.join(" ");
